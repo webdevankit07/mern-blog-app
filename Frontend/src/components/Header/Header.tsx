3c1f20002fc94c95ba652641cc60ea9e72359a1a
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-02-16 16:27:09 +0530

    fix header

--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -75,9 +75,6 @@ const Header = () => {
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
-            <Button className='w-12 h-10 lg:hidden' color='gray' pill>
-                <AiOutlineSearch />
-            </Button>
             <div className='flex gap-2 md:order-2'>
                 <Button className='w-12 h-10' color='gray' pill onClick={() => dispatch(toggleTheme())}>
                     {theme === 'dark' ? <FaMoon /> : <FaSun />}
