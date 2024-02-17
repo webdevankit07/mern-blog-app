f2710479c31ac31382969410b8cf2e9dab0145bc
Ankit Yadav <126321571+webdevankit07@users.noreply.github.com>
2024-02-17 08:04:14 +0530

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8000',
+                target: 'https://webdevankit-blog-app.vercel.app',
                 secure: false,
             },
         },
