1a5420f5c91354e300411869113bbf529080083c
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-02-17 08:16:18 +0530

    fix deployement bug

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,12 +6,8 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-<<<<<<< HEAD
                 // target: 'https://webdevankit-blog-app.vercel.app',
                 target: 'http://localhost:8000',
-=======
-                target: 'https://webdevankit-blog-app.vercel.app',
->>>>>>> f2710479c31ac31382969410b8cf2e9dab0145bc
                 secure: false,
             },
         },
