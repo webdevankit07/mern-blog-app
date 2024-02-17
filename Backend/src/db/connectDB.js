3e8b4e3accee3eb5e2ffce935a11ed1844af9dbd
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-02-17 11:43:02 +0530

    fix bug

--- Backend/src/db/connectDB.js
+++ Backend/src/db/connectDB.js
@@ -1,5 +1,7 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
 
 const connectDB = async () => {
     try {
