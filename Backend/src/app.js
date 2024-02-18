0525ea4a4e4bb57ca7cd1da56168071d2f80c0fd
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-02-18 10:37:11 +0530

    fix backend api

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { authRoutes } from './routes/auth.routes.js';
 import { userRoutes } from './routes/user.routes.js';
 import { postRoutes } from './routes/post.routes.js';
@@ -10,20 +8,28 @@ import { commentRoutes } from './routes/comment.routes.js';
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:5173',
+        credentials: true,
+    })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(cookieParser());
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, '../../Frontend/dist')));
 
 // Routes declaration...
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/comment', commentRoutes);
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 'ok',
+        message: 'I am home route. Sever is live',
+    });
+});
 
 export default app;
