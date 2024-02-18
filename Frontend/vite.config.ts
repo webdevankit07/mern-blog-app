f645c9f2a4d6cf556a8a76f4efb7cd1957e28f98
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-02-18 09:56:05 +0530

    Revert "fix deployement bug"
    This reverts commit 1a5420f5c91354e300411869113bbf529080083c.

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -6,8 +6,12 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
+<<<<<<< HEAD
                 // target: 'https://webdevankit-blog-app.vercel.app',
                 target: 'http://localhost:8000',
+=======
+                target: 'https://webdevankit-blog-app.vercel.app',
+>>>>>>> f2710479c31ac31382969410b8cf2e9dab0145bc
                 secure: false,
             },
         },
